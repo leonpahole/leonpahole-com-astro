b348fc57d473daea9b54a376a7cbdfeb9346db1f
Leon Pahole <leon.pahole@gmail.com>
2023-09-24 11:33:52 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
+import tailwind from "@astrojs/tailwind";
+import { astroImageTools } from "astro-imagetools";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-import tailwind from "@astrojs/tailwind";
-import { astroImageTools } from "astro-imagetools";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), astroImageTools, sitemap()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     syntaxHighlight: "prism",
   },
+  site: "https://www.leonpahole.com",
 });
