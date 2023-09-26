c16b43edbd43285a4bd020df85adea479cbdb790
Leon Pahole <leon.pahole@gmail.com>
2023-09-26 14:06:18 +0200

    Move site to build env

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,21 @@
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { astroImageTools } from "astro-imagetools";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, sitemap()],
+  integrations: [tailwind(), astroImageTools, sitemap(), robotsTxt()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
     syntaxHighlight: "prism",
   },
-  site: "https://www.leonpahole.com",
+  site: process.env.SITE,
+  vite: {
+    optimizeDeps: {
+      exclude: ["astro-imagetools"],
+    },
+  },
 });
