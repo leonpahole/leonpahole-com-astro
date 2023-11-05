d2b2547dbb333b263aa0b7327f1cee365dacb4d0
Leon Pahole <leon.pahole@gmail.com>
2023-11-05 07:44:35 +0100

    Change CV link

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,7 +1,7 @@
 export const PersonalTitle = "Leon Pahole - Software Engineer";
 
 export const PersonalDescription =
-  "I'm a software engineer with management experience and strong soft skills.";
+  "I'm a full-stack web developer, lead and mentor.";
 
 export const BlogDescription =
   "I write about web development, soft skills, health, and productivity.";
